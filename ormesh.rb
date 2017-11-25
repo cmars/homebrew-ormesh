3ecbaad0bfc22f6b0429f6cdcc08031dc50b4076
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-24 22:57:58 -0600

    ormesh version v0.1.7

---
+++ ormesh.rb
@@ -0,0 +1,15 @@
+class Ormesh < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cmars/ormesh/releases/download/v0.1.7/ormesh_0.1.7_darwin_amd64.tar.gz"
+  version "0.1.7"
+  sha256 "6dbb17df5a10cecb068b2195b336a7541968d632335e5945de1517fdb8756941"
+
+  def install
+    bin.install "ormesh"
+  end
+
+  test do
+    
+  end
+end
